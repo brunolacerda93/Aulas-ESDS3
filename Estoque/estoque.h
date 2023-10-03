737e1f0d8a7d46a6f4bf646211c82fa0836b66cf
Bruno Lacerda <91638794+brunolacerda93@users.noreply.github.com>
2023-10-03 13:58:15 -0300

    Update estoque.h

--- Estoque/estoque.h
+++ Estoque/estoque.h
@@ -7,6 +7,7 @@
     #define NOME_LEN 100
     #define PRODUTOS "PRODUTOS"
     #define DEFAULT_SIZE 10
+    
     typedef unsigned int natural;
 
 //-------------------------------------------------------------------------------------------------------------//
@@ -14,201 +15,80 @@
     // Estrutura dos tipos
 
     typedef struct Produto {
+
         natural codigo;
         char    nome [NOME_LEN];
         natural quantidade;
         double  preco;
         struct  Produto* proximo;
+
     } Produto;
 
     typedef struct Lista_de_Produtos {
+
         Produto* primeiro;
         Produto* ultimo;
         size_t   tamanho;
         size_t   max;
+
     } Lista_de_Produtos;
 
 //-------------------------------------------------------------------------------------------------------------//
 
     // Construtores
 
-    //
-    // Retorna um Produto não preenchido
-    //
-    Produto* CriaProduto();
-
-    //
-    // Retorna um Produto preenchido
-    //
-    Produto* CriaProdutoArgs(natural cod,
-                             string nome,
-                             natural qtde,
-                             double preco);
-
-    //
-    // Retorna uma Lista de Produtos
-    //
+    Produto*           CriaProduto();
+    Produto*           CriaProdutoArgs(natural, string, natural, double);
     Lista_de_Produtos* CriaListaProdutos();
-
-    //
-    // Retorna uma Lista de Produtos com tamanho máximo definido
-    //
-    Lista_de_Produtos* CriaListaProdutosArgs(size_t max);
+    Lista_de_Produtos* CriaListaProdutosArgs(size_t);
 
 //-------------------------------------------------------------------------------------------------------------//
 
     // Métodos úteis
 
-    //
-    // Exibe um Produto formatado para o console
-    //
-    void ExibeProduto(Produto* produto);
-
-    //
-    // Retorna um ponteiro para o Produto com o código passado, ou NULL
-    //
-    Produto* ProdutoPorCodigo(Lista_de_Produtos* lista, natural cod);
-    
-    //
-    // Retorna um ponteiro para o Produto com o nome passado, ou NULL
-    //
-    Produto* ProdutoPorNome(Lista_de_Produtos* lista, string nome);
-
-    //
-    // Exibe uma lista de Produtos com quantidade menor ou igual à informada
-    //
-    void ExibeProdutosPorQtde(Lista_de_Produtos* lista);
-
-    //
-    // Exibe uma lista de Produtos com preço menor ou igual ao informado
-    //
-    void ExibeProdutosPorPreco(Lista_de_Produtos* lista);
-
-    //
-    // Retorna uma cópia profunda de um Produto
-    //
-    Produto* ClonaProduto(Produto* produto);
-
-    //
-    // Retorna true se o nome digitado for válido
-    //
-    bool NomeInput(string nome);
-
-    //
-    // Tela para administrar a exibição de um ou mais Produtos
-    //
-    void TelaProdutos(Lista_de_Produtos* lista);
+    bool     NomeInput             (string);
+    void     TelaProdutos          (Lista_de_Produtos*);
+    void     ExibeProduto          (Produto*);
+    void     ExibeProdutosPorQtde  (Lista_de_Produtos*);
+    void     ExibeProdutosPorPreco (Lista_de_Produtos*);
+    Produto* ClonaProduto          (Produto*);
+    Produto* ProdutoPorNome        (Lista_de_Produtos*, string);
+    Produto* ProdutoPorCodigo      (Lista_de_Produtos*, natural);
 
 //-------------------------------------------------------------------------------------------------------------//
 
     // CRUD
 
-    //
-    // Procura um Produto com o código informado e exibe na tela, ou simplesmente não exibe nada
-    //
-    void ExibeProdutoPorCodigo(Lista_de_Produtos* lista);
-
-    //
-    // Procura um Produto com o nome informado e exibe na tela, ou simplesmente não exibe nada
-    //
-    void ExibeProdutoPorNome(Lista_de_Produtos* lista);
-
-    //
-    // Exibe toda uma Lista de Produtos
-    //
-    void ExibeTodosProdutos(Lista_de_Produtos* lista);
-
-    //
-    // Retorna um novo produto cadastrado, ou NULL
-    //
-    Produto* NovoProduto(Lista_de_Produtos* lista);
-
-    //
-    // Insere um Produto na última posição da lista
-    //
-    void InsereProdutoNalista(Lista_de_Produtos* lista, Produto* produto);
-
-    //
-    // Encapsula a inserção na Lista
-    //
-    void Insereproduto(Lista_de_Produtos* lista);
-
-    //
-    // Atualiza as informações de um Produto com o codigo informado
-    //
-    void AtualizaProduto(Lista_de_Produtos* lista);
-    
-    //
-    // Remove um produto com o codigo informado
-    //
-    void RemoveProduto(Lista_de_Produtos* lista);
+    void     ExibeProdutoPorCodigo (Lista_de_Produtos*);
+    void     ExibeProdutoPorNome   (Lista_de_Produtos*);
+    void     ExibeTodosProdutos    (Lista_de_Produtos*);
+    void     Insereproduto         (Lista_de_Produtos*);
+    void     AtualizaProduto       (Lista_de_Produtos*);
+    void     RemoveProduto         (Lista_de_Produtos*);
+    void     InsereProdutoNalista  (Lista_de_Produtos*, Produto*);
+    Produto* NovoProduto           (Lista_de_Produtos*);
 
 //-------------------------------------------------------------------------------------------------------------//
 
     // Ordenação
 
-    //
-    // Bubble Sort que ordena o Estoque por codigo de forma crescente
-    //
-    void bubbleSort_codigo(Lista_de_Produtos* lista);
-
-    //
-    // Bubble Sort que ordena o Estoque por nome em ordem alfabética
-    //
-    void bubbleSort_nome(Lista_de_Produtos* lista);
-
-    //
-    // Bubble Sort que ordena o Estoque por quantidade de forma crescente
-    //
-    void bubbleSort_qtde_c(Lista_de_Produtos* lista);
-
-    //
-    // Bubble Sort que ordena o Estoque por quantidade de forma decrescente
-    //
-    void bubbleSort_qtde_d(Lista_de_Produtos* lista);
-
-    //
-    // Bubble Sort que ordena o Estoque por preco de forma crescente
-    //
-    void bubbleSort_preco_c(Lista_de_Produtos* lista);
-
-    //
-    // Bubble Sort que ordena o Estoque por preco de forma decrescente
-    //
-    void bubbleSort_preco_d(Lista_de_Produtos* lista);
-
-    //
-    // Encapsula a ordenação
-    //
-    void ExibeOrdenado(Lista_de_Produtos* lista);
+    void bubbleSort_codigo  (Lista_de_Produtos*);
+    void bubbleSort_nome    (Lista_de_Produtos*);
+    void bubbleSort_qtde_c  (Lista_de_Produtos*);
+    void bubbleSort_qtde_d  (Lista_de_Produtos*);
+    void bubbleSort_preco_c (Lista_de_Produtos*);
+    void bubbleSort_preco_d (Lista_de_Produtos*);
+    void ExibeOrdenado      (Lista_de_Produtos*);
 
 //-------------------------------------------------------------------------------------------------------------//
 
     // Manipulação de Arquivos
 
-    //
-    // Registra Produtos em um arquivo PRODUTOS
-    //
-    void PersistenciaProdutos(Lista_de_Produtos* lista);
-
-    //
-    // Lê o arquivo PRODUTOS e retorna uma lista de Produtos de tamanho padrão com os dados obtidos
-    //
+    void PersistenciaProdutos(Lista_de_Produtos*);
     Lista_de_Produtos* ReadProdutos();
 
 //-------------------------------------------------------------------------------------------------------------//
 
-    //
-    // Submenu de produtos
-    //
-    void MenuProdutos(Lista_de_Produtos* lista);
-
-    //
-    // Breve Menu para salvar ou não as alterações
-    //
-    int LogOff(Lista_de_Produtos* lista);
-
-    //
-    // Apaga completamente uma lista de Produtos
-    //
-    void FreeEstoque(Lista_de_Produtos* lista);
+    int  LogOff       (Lista_de_Produtos*);
+    void MenuProdutos (Lista_de_Produtos*);
+    void FreeEstoque  (Lista_de_Produtos*);
