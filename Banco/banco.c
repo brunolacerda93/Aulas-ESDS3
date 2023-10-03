b38dc53d17207b9a9a93e4477e11a8ea707b4bc6
Bruno Lacerda <91638794+brunolacerda93@users.noreply.github.com>
2023-10-03 14:21:17 -0300

    Update banco.c

--- Banco/banco.c
+++ Banco/banco.c
@@ -17,3 +17,28 @@
     //
     // Subemnu do Banco
     //
+    void MenuBanco(Fila_Banco* fila, Fila_Banco* fila_geral, Fila_Banco* fila_pref) {
+        int opc;
+
+        do { // hast
+            cleanScreen();
+
+            puts("=========================");
+            puts("BANCO\n");
+            puts(" 1 - Entrar na Fila");
+            puts(" 2 - Exibir Fila");
+            puts(" 3 - Andar Fila");
+            puts(" 0 - Sair\n");
+            printf("Escolha: ");
+            opc = getchar(); clearBuffer();
+
+            switch (opc) {
+                case '1':
+                case '2':
+                case '3':
+                case '0': pause(); return;
+                default : break;
+            }
+
+        } while (opc != '0');
+    }
